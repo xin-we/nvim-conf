5f92c993487d78c379d8554c5f84474e250ca44d
xin-we <wxnicelife@gmail.com>
2023-05-30 21:29:36 +0900

    init project

---
+++ init.lua
@@ -0,0 +1,21 @@
+local set = vim.o
+set.number = true
+set.relativenumber = true
+set.clipboard = "unnamed"
+
+-- highlight after copy
+vim.api.nvim_create_autocmd({ "TextYankPost" }, {
+	pattern = { "*" },
+	callback = function()
+		vim.highlight.on_yank({
+			timeout = 300,
+		})
+	end,
+})
+
+-- keybindings
+local opt = { noremap = true, silent = true }
+vim.keymap.set("n", "<C-l>", "<C-w>l", opt)
+vim.keymap.set("n", "<C-h>", "<C-w>h", opt)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opt)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opt)
