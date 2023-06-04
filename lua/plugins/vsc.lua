be26d85bbd38607fb2f9b7b9852526e5fe9b4ff3
xin-we <wxnicelife@gmail.com>
2023-06-04 16:35:41 +0900

    add gitsigns.nvim

--- lua/plugins/vsc.lua
+++ lua/plugins/vsc.lua
@@ -1,5 +1,13 @@
 return {
 	{
+		"lewis6991/gitsigns.nvim",
+		event = "VeryLazy",
+		commit = "addd6e174a85fc1c4007ab0b65d77e6555b417bf",
+		config = function()
+			require("gitsigns").setup({})
+		end,
+	},
+	{
 		event = "VeryLazy",
 		"tpope/vim-fugitive",
 		config = function() end,
